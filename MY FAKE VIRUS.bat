3acc00d022805c525f9f928846634b8a51ef57fa
T3chyy <34737526+RedAl3rtT3chyy@users.noreply.github.com>
2020-04-04 10:29:15 -0500

    Add files via upload

---
+++ MY FAKE VIRUS.bat
@@ -0,0 +1,19 @@
+@echo off
+echo --------------------------------------------------------------------
+echo WARNING! A VIRUS HAS BEEN DETECTED ON YOUR COMPUTER!
+pause;
+echo FILE RECOGNIZED AS SISDPYGnfg32.bat
+pause;
+echo CONTACTING ANTIVIRUS...
+pause;
+echo WOULD YOU LIKE TO DELETE THE VIRUS? (y/n)
+pause;
+echo ANTIVIRUS HAS BEEN MANUALLY SHUT DOWN!
+pause;
+echo VIRUS HAS PROGRESSED TO C:/ DRIVE!
+pause;
+echo FILES IN C:/ WILL BE DELETED!
+pause;
+:a
+dir C:\
+goto :a
